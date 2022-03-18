846879717e3df3951474e7be5d8c634d98084360
changwoo <ep6tri@hotmail.com>
2022-03-18 12:59:37 +0900

    Remove temporary code

--- includes/registers/class-nbpc-register-post-type.php
+++ includes/registers/class-nbpc-register-post-type.php
@@ -12,13 +12,6 @@ if ( ! class_exists( 'NBPC_Register_Post_Type' ) ) {
 	class NBPC_Register_Post_Type extends NBPC_Register_Base_Post_Type {
 		public function get_items(): Generator {
 			yield; // yield new NBPC_Reg_Post_Type();
-
-			yield new NBPC_Reg_Post_Type( 'article', [
-				'label'        => 'Article',
-				'public'       => true,
-				'has_archive'  => true,
-				'hierarchical' => false,
-			] );
 		}
 	}
 }
--- includes/registers/class-nbpc-register-taxonomy.php
+++ includes/registers/class-nbpc-register-taxonomy.php
@@ -12,15 +12,6 @@ if ( ! class_exists( 'NBPC_Register_Taxonomy' ) ) {
 	class NBPC_Register_Taxonomy extends NBPC_Register_Base_Taxonomy {
 		public function get_items(): Generator {
 			yield; // yield new NBPC_Reg_Taxonomy();
-			yield new NBPC_Reg_Taxonomy(
-				'article_cat',
-				[ 'article' ],
-				[
-					'label'        => 'Article Cat',
-					'public'       => true,
-					'hierarchical' => true,
-				]
-			);
 		}
 	}
 }
