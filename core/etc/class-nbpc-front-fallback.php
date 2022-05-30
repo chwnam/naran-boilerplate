aafd899cb752692c075be6f5bb7ad0a48bcd7522
changwoo <ep6tri@hotmail.com>
2022-05-30 16:39:13 +0900

    Remove heredoc

--- core/etc/class-nbpc-front-fallback.php
+++ core/etc/class-nbpc-front-fallback.php
@@ -12,67 +12,67 @@ if ( ! class_exists( 'NBPC_Front_Fallback' ) ) {
 		use NBPC_Template_Impl;
 
 		public function display(): void {
-echo <<< 'EOT'
-<style>
-    #nbpc-fallback-wide {
-        width: 100%;
-        background-color: #eee;
-        font-family: "Ubuntu Mono", monospace;
-        font-size: 12pt;
-        color: #191919;
-        margin-top: 50px;
-        margin-bottom: 50px;
-    }
+			?>
+            <style>
+                #nbpc-fallback-wide {
+                    width: 100%;
+                    background-color: #eee;
+                    font-family: "Ubuntu Mono", monospace;
+                    font-size: 12pt;
+                    color: #191919;
+                    margin-top: 50px;
+                    margin-bottom: 50px;
+                }
 
-    #nbpc-fallback-wide .nbpc-fallback-container {
-        width: 640px;
-        margin: 0 auto 0;
-        padding: 20px 10px 20px;
-        background-color: #ddd;
-    }
+                #nbpc-fallback-wide .nbpc-fallback-container {
+                    width: 640px;
+                    margin: 0 auto 0;
+                    padding: 20px 10px 20px;
+                    background-color: #ddd;
+                }
 
-    #nbpc-fallback-wide .nbpc-fallback-title {
-        width: 100%;
-        text-align: center;
-    }
+                #nbpc-fallback-wide .nbpc-fallback-title {
+                    width: 100%;
+                    text-align: center;
+                }
 
-    #nbpc-fallback-wide .nbpc-fallback-instruction pre.bold {
-        display: inline-block;
-        background-color: #b3b3b3;
-        font-weight: bold;
-    }
+                #nbpc-fallback-wide .nbpc-fallback-instruction pre.bold {
+                    display: inline-block;
+                    background-color: #b3b3b3;
+                    font-weight: bold;
+                }
 
-    #nbpc-fallback-wide pre,
-    #nbpc-fallback-wide code {
-        font-family: "Ubuntu Mono", monospace;
-        font-size: 10pt;
-    }
+                #nbpc-fallback-wide pre,
+                #nbpc-fallback-wide code {
+                    font-family: "Ubuntu Mono", monospace;
+                    font-size: 10pt;
+                }
 
-    #nbpc-fallback-wide pre.code {
-        background-color: #aaa;
-        padding: 10px 15px;
-        margin-right: 35px;
-    }
+                #nbpc-fallback-wide pre.code {
+                    background-color: #aaa;
+                    padding: 10px 15px;
+                    margin-right: 35px;
+                }
 
-</style>
+            </style>
 
-<div id="nbpc-fallback-wide">
-    <div class="nbpc-fallback-container">
-        <h1 class="nbpc-fallback-title">Fallback template</h1>
-        <p>
-            Front module is not properly set up. Please follow the instructions below:
-        </p>
-        <ol class="nbpc-fallback-instruction">
-            <li>Open
-                <pre class="bold">`class-nbpc-register-theme-support.php`</pre>
-                file.
-            </li>
-            <li>Search for
-                <pre class="bold">`NBPC_Register_Theme_Support::map_front_modules()`</pre>
-                method.
-            </li>
-            <li>Set up the front module, like:
-                <pre class="code"><code>public function map_front_modules( WP_Query $query ) {
+            <div id="nbpc-fallback-wide">
+                <div class="nbpc-fallback-container">
+                    <h1 class="nbpc-fallback-title">Fallback template</h1>
+                    <p>
+                        Front module is not properly set up. Please follow the instructions below:
+                    </p>
+                    <ol class="nbpc-fallback-instruction">
+                        <li>Open
+                            <pre class="bold">`class-nbpc-register-theme-support.php`</pre>
+                            file.
+                        </li>
+                        <li>Search for
+                            <pre class="bold">`NBPC_Register_Theme_Support::map_front_modules()`</pre>
+                            method.
+                        </li>
+                        <li>Set up the front module, like:
+                            <pre class="code"><code>public function map_front_modules( WP_Query $query ) {
     if ( ! $query->is_main_query() ) {
         return;
     }
@@ -88,18 +88,15 @@ echo <<< 'EOT'
         $hierarchy->set_front_module( Singular_Front_Module::class );
     }
 }</code></pre>
-            </li>
-            <li>You can also override the state of your theme hierarchy instance by action
-                <pre class="bold">'nbpc_theme_hierarchy'</pre>
-                .
-                See
-                <pre class="bold">`NBPC_Theme_Hierarchy::__construct()`</pre>
-                .
-            </li>
-        </ol>
-    </div>
-</div>
-EOT;
+                        </li>
+                        <li>You can also override the state of your theme hierarchy instance by action
+                            <pre class="bold">'nbpc_theme_hierarchy'</pre>.
+                            See <pre class="bold">`NBPC_Theme_Hierarchy::__construct()`</pre>.
+                        </li>
+                    </ol>
+                </div>
+            </div>
+			<?php
 		}
 	}
 } 
