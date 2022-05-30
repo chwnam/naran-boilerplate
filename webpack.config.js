62bb8b0f3273645717cbf5357ee0957714eafbc6
changwoo <ep6tri@hotmail.com>
2022-05-30 16:04:10 +0900

    Add default webpack.config.js

---
+++ webpack.config.js
@@ -0,0 +1,33 @@
+const defaultConfig = require('@wordpress/scripts/config/webpack.config')
+const {hasArgInCLI} = require('@wordpress/scripts/utils')
+const MiniCSSExtractPlugin = require('mini-css-extract-plugin')
+
+const isProduction = process.env.NODE_ENV === 'production'
+const hasReactFastRefresh = hasArgInCLI('--hot') && !isProduction
+
+if (!isProduction) {
+    // Modify MiniCSSExtractPlugin to create css files in the 'assets/css/dist' directory.
+    const plugins = defaultConfig.plugins,
+        pluginIdx = plugins.findIndex(plugin => plugin instanceof MiniCSSExtractPlugin)
+
+    if (pluginIdx > -1) {
+        const newPlugin = new MiniCSSExtractPlugin({
+            filename: '../../css/dist/[name].css',
+        })
+        plugins.splice(pluginIdx, 1, newPlugin)
+    }
+
+    module.exports = {
+        ...defaultConfig,
+        // To support HMR (Hot Module Replacement) feature.
+        devServer: {
+            ...defaultConfig.devServer,
+            allowedHosts: [
+                '127.0.0.1',
+                'localhost',
+                '.dev.site', // NOTE: Modify it to your development server domain. A leading dot indicates subdomain wildcard.
+            ],
+            hot: hasReactFastRefresh,
+        },
+    }
+}
