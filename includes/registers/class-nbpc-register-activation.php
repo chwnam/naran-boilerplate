4e75ff1065fa1b9d17d37da880545f4407e2eb0e
changwoo <ep6tri@hotmail.com>
2022-06-13 11:07:53 +0900

    Remove items of install register, and uninstall register

--- includes/registers/class-nbpc-register-activation.php
+++ includes/registers/class-nbpc-register-activation.php
@@ -11,14 +11,7 @@ if ( ! defined( 'ABSPATH' ) ) {
 if ( ! class_exists( 'NBPC_Register_Activation' ) ) {
 	class NBPC_Register_Activation extends NBPC_Register_Base_Activation {
 		public function get_items(): Generator {
-			// Add defined roles
-			yield new NBPC_Reg_Activation( 'registers.role@register' );
-
-			// Add defined caps
-			yield new NBPC_Reg_Activation( 'registers.cap@register' );
-
-			// Add custom tables
-			yield new NBPC_Reg_Activation( 'registers.custom_table@register' );
+			yield; // new NBPC_Reg_Activation( 'registers.role@register' );
 		}
 	}
 }
--- includes/registers/class-nbpc-register-uninstall.php
+++ includes/registers/class-nbpc-register-uninstall.php
@@ -11,7 +11,7 @@ if ( ! defined( 'ABSPATH' ) ) {
 if ( ! class_exists( 'NBPC_Register_Uninstall' ) ) {
 	class NBPC_Register_Uninstall extends NBPC_Register_Base_Uninstall {
 		public function get_items(): Generator {
-			yield new NBPC_Reg_Uninstall( 'registers.custom_table@unregister' );
+			yield; // yield new NBPC_Reg_Uninstall();
 		}
 	}
 }
